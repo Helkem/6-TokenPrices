558336229e5022d71d3cbbf2937691303b27a39b
helkem0 <helkem0@protonmail.com>
2024-09-07 20:39:37 +0800

    Footer correction

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -1,13 +1,13 @@
 export default function Footer() {
   return (
-    <footer className='mt-80 mb-5 w-full flex justify-center'>
+    <footer className='mt-[590px] mb-5 w-full flex justify-center'>
       <a
         className='text-[12px] text-gray-700 hover:text-black transition-all duration-150 ease-in-out'
         href='https://github.com/Helkem'
         target='_blank'
         rel='noopener noreferrer'
       >
-        https://github .com/Helkem
+        https://github.com/Helkem
       </a>
     </footer>
   );
