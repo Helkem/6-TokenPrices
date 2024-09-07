740f888703038502a145d0598f449208dd6f3db2
helkem0 <helkem0@protonmail.com>
2024-09-07 22:40:06 +0800

    site logo fix

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header>
       <h1>
-        <img src={"/logo2.svg"} alt='logo' className='w-51 h-10' />
+        <img src={"/sitelogo.png"} alt='logo' className='w-51 h-10' />
       </h1>
       <p className='text-sm text-gray-500 mb-6 mt-2 font-medium'>
         Track the latest prices, market caps, and more for your favorite
